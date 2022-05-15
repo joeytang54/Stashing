9363c41048cae6bf6e6c57240f26f8ebb55d6f44
joeytang54 <joeytang54@gmail.com>
2022-05-15 12:51:35 -0400

    setting to 80MHZ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,16 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        basicFeatures car=new basicFeatures(); 
+        basicFeatures Audi=new basicFeatures();
+        Audi.color="red";
+        Audi.doors="4";
+        Audi.enginCapac="V8";
+        Audi.headLights="blue";
+        Audi.regNo="JOEY"; 
+
+        System.out.println("Welcome to my car");
+        System.out.println("The radio is setup to 80MZH");
+        System.out.println("turn on the air condition");
     }
+
 }
